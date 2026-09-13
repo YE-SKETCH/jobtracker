78cbc35f0d977afb51d11024f3159c781b7e84a2
jusd6938-art <your-real-github-email@example.com>
2026-09-14 00:34:52 +0530

    crud

--- src/main/java/com/example/jobtracker/controller/JobController.java
+++ src/main/java/com/example/jobtracker/controller/JobController.java
@@ -1,4 +1,36 @@
 package com.example.jobtracker.controller;
 
+import com.example.jobtracker.model.Job;
+import com.example.jobtracker.service.JobService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class JobController {
+    private final JobService jobService;
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+    @PostMapping("/jobs")
+public Job createJob(@RequestBody Job job) {
+    return jobService.createJob(job);
+}
+    @GetMapping("/jobs")
+    public List<Job> getAllJobs() {
+        return jobService.getAllJobs();
+    }
+    @GetMapping("/jobs/{id}")
+    public Optional<Job> getJobById(@PathVariable Long id) {
+        return jobService.getJobById(id);
+    }
+    @PutMapping("/jobs/{id}")
+    public Job updateJob(@PathVariable Long id, @RequestBody Job job) {
+        return jobService.updateJob(id, job);
+    }
+    @DeleteMapping("/jobs/{id}")
+    public void deleteJob(@PathVariable Long id) {
+        jobService.deleteJob(id);
+    }
 }
--- src/main/java/com/example/jobtracker/service/JobService.java
+++ src/main/java/com/example/jobtracker/service/JobService.java
@@ -1,4 +1,37 @@
 package com.example.jobtracker.service;
 
+import com.example.jobtracker.model.Job;
+import com.example.jobtracker.repository.JobRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class JobService {
+    private final JobRepository jobRepository;
+    public JobService(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+    public Job createJob(Job job) {
+        return jobRepository.save(job);
+    }
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
+    }
+    public Optional<Job> getJobById(Long id) {
+        return jobRepository.findById(id);
+    }
+    public Job updateJob(Long id, Job job) {
+        Job existingJob = jobRepository.findById(id).orElseThrow();
+
+        existingJob.setCompany(job.getCompany());
+        existingJob.setRole(job.getRole());
+        existingJob.setCity(job.getCity());
+
+        return jobRepository.save(existingJob);
+    }
+    public void deleteJob(Long id) {
+        jobRepository.deleteById(id);
+    }
 }
